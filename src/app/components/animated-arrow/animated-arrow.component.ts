5b560ba39e45bd8e3f6ebec50cfa427a1281562f
enzo teixeira <enzo.teix73@gmail.com>
2025-02-16 13:14:53 +0100

    IMPL - Home page - Text, style and a scroll button arrow

---
+++ src/app/components/animated-arrow/animated-arrow.component.ts
@@ -0,0 +1,78 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core'
+import {NgClass} from '@angular/common';
+
+@Component({
+  selector: 'animated-arrows',
+  styles: [`
+    .arrow-button {
+      border: none;
+      display: flex;
+      justify-content: center;
+      background: none;
+      width: 60px;
+      height: 60px;
+      position: absolute;
+    }
+
+    .arrow-button:focus {
+      outline: 0;
+      background: none;
+      box-shadow: none;
+    }
+
+    .arrow-button:hover {
+      background: none;
+      cursor: pointer;
+    }
+
+    .arrow-button .arrow {
+      box-shadow: 7px 7px 0 0 var(--mat-sys-secondary) inset;
+      padding: 12px;
+      display: flex;
+      position: absolute;
+      justify-content: center;
+
+    }
+
+    .arrow-button .arrow.down {
+      transform: rotate(-135deg);
+    }
+
+    .arrow-button .arrow-one.down {
+      animation: arrow-movement-down 2s ease infinite;
+    }
+
+
+    @keyframes arrow-movement-down {
+      0% {
+        top: 0;
+      }
+      40% {
+        top: 30px;
+      }
+      100% {
+        top: 0;
+      }
+    }
+
+  `],
+  imports: [
+    NgClass
+  ],
+  template: `
+    <button class="arrow-button" (click)="onButtonClick()" >
+      <div [ngClass]="'arrow arrow-one ' + direction">
+      </div>
+    </button>
+  `
+})
+export class AnimatedArrowsButtonComponent {
+  @Input()
+  direction = '';
+  @Output('onClick')
+  handleClick = new EventEmitter;
+
+  onButtonClick() {
+    this.handleClick.emit();
+  }
+}
