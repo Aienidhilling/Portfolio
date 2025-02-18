57a03bd6d406ce131c8e33c0e85ef6cfa9db0a7b
enzo teixeira <enzo.teix73@gmail.com>
2025-02-18 22:46:09 +0100

    FIX - Style - default background theme depend now on the background theme selected in the user browser

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -5,15 +5,30 @@ import {DOCUMENT} from '@angular/common';
   providedIn: 'root'
 })
 export class ThemeService {
-  private isDarkMode = false;
   private document = inject(DOCUMENT);
 
+  private isDarkTheme: boolean = false;
+
+  constructor() {
+    this.detectBrowserTheme();
+  }
+
+  detectBrowserTheme() {
+    this.isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.applyTheme();
+  }
+
+
   toggleTheme():void{
-    this.isDarkMode = !this.isDarkMode;
+    this.isDarkTheme = !this.isDarkTheme;
     this.applyTheme();
   }
 
   private applyTheme():void{
-    this.document.body.classList.toggle('dark');
+    if (this.isDarkTheme){
+      this.document.body.classList.add('dark');
+    }else {
+      this.document.body.classList.remove('dark');
+    }
   }
 }
